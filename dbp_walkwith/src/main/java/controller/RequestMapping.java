2ac6b793711214529c9646c00c5214d9c9a73e1d
mhyeon-kim <0214kkkk@naver.com>
2023-12-17 21:00:42 +0900

    필요없는 파일 정리, 필요한 파일 생성

--- dbp_walkwith/src/main/java/controller/RequestMapping.java
+++ dbp_walkwith/src/main/java/controller/RequestMapping.java
@@ -5,9 +5,6 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import controller.user.*;
-import controller.comm.*;
-
 public class RequestMapping {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
     
